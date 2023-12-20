5ef4be4a717b4b09dd0a3939faf13bb733065bed
cypripricheche <cyprien.chevillotte@gmail.com>
2023-12-20 17:15:17 +0100

    Fichier Controller

--- src/main/java/com/llm/agimmoweb/controller/AgImmoWebController.java
+++ src/main/java/com/llm/agimmoweb/controller/AgImmoWebController.java
@@ -1,5 +1,22 @@
-package com.llm.agimmoweb.controller;
+ package com.llm.agimmoweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AgImmoWebController {
+    @GetMapping(value = "/")
+    public String callHome() {
+        return "notreEquipe";
+    }
 
+    @GetMapping(value = "/equipe")
+    public String callEquipePage() {
+        return "notreEquipe";
+    }
+    
+    @GetMapping(value = "/agences")
+    public String callAgencePage() {
+        return "nosAgences";
+    }
 }
